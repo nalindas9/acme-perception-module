8b86272d4c23bf9540fbdea34269e714707c8680
Nalin Das <nalindas@gmail.com>
2020-10-20 16:40:31 -0400

    [Task 1.12] Modify YolovV3 stub
    [app/YoloV3.cpp] Returned stub methods constant values

--- app/YoloV3.cpp
+++ app/YoloV3.cpp
@@ -13,35 +13,63 @@
  */
 #include "YoloV3.hpp"
 
-void mtp::YoloV3::setConfThreshold(double conf_threshold_value) {}
+void mtp::YoloV3::setConfThreshold(double conf_threshold_value) {
+    confThreshold = 0.6;
+}
 
 double mtp::YoloV3::getConfThreshold() {
-    return 0.25;
+    return 0.6;
 }
 
-void mtp::YoloV3::setNmsThreshold(double nms_threshold_value) {}
+void mtp::YoloV3::setNmsThreshold(double nms_threshold_value) {
+    nmsThreshold = 0.2;
+}
 
 double mtp::YoloV3::getNmsThreshold() {
-    return 0.5;
+    return 0.2;
 }
 
-void mtp::YoloV3::setInputHeight(int input_height_value) {}
+void mtp::YoloV3::setInputHeight(int input_height_value) {
+    inputHeight = 32;
+}
 
 int mtp::YoloV3::getInputHeight() {
-    return 
+    return 32;
 }
 
-void mtp::YoloV3::setInputWidth(int input_width_value);
+void mtp::YoloV3::setInputWidth(int input_width_value) {
+    inputWidth = 32;
+}
 
-int mtp::YoloV3::getInputWidth();
+int mtp::YoloV3::getInputWidth() {
+    return 32;
+}
 
-cv::Mat mtp::YoloV3::preProcess(const cv::Mat& frame);
+cv::Mat mtp::YoloV3::preProcess(const cv::Mat& frame) {
+    cv::Mat output_blob(416, 416, CV_8UC3, cv::Scalar(0,0,255));
+    return output_blob; 
+}
 
-cv::Mat mtp::YoloV3::postProcess(const cv::Mat& frame);
+cv::Mat mtp::YoloV3::postProcess(const cv::Mat& frame) {
+    cv::Mat output_blob(416, 416, CV_8UC3, cv::Scalar(0,0,255));
+    return output_blob; 
+}
 
-cv::Mat mtp::YoloV3::runInference(const cv::Mat& frame);
+cv::Mat mtp::YoloV3::runInference(const cv::Mat& frame) {
+    cv::Mat output_blob(416, 416, CV_8UC3, cv::Scalar(0,0,255));
+    return output_blob; 
+}
 
-void mtp::YoloV3::YoloV3(double conf_threshold_value, 
+mtp::YoloV3::YoloV3(double conf_threshold_value, 
                 double nms_threshold_value,
                 int input_width_value,
-                int input_height_value); 
+                int input_height_value) {
+    confThreshold = conf_threshold_value;
+    nmsThreshold = nms_threshold_value;
+    inputHeight = input_height_value;
+    inputWidth = input_width_value;
+}
+
+mtp::YoloV3::YoloV3() {};
+
+mtp::YoloV3::~YoloV3() {};
