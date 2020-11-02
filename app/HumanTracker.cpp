6ece6becef983a985b1dafe46d175653674eb599
Nalin Das <nalindas9@gmail.com>
2020-11-02 18:50:19 -0500

    [Task 2.2] Fix travis and code coverage issue
    [app/utils.cpp] Remove pass by reference
    
    [app/HumanTracker.cpp] Remove pass by reference
    
    [CMakeLists.txt] Change test executable name

--- app/HumanTracker.cpp
+++ app/HumanTracker.cpp
@@ -18,7 +18,7 @@ std::string mtp::HumanTracker::getRobotFrame() {
     return "new_robot_frame";
 }
 
-void mtp::HumanTracker::setRobotFrame(std::string &robot_frame) {
+void mtp::HumanTracker::setRobotFrame(std::string robot_frame) {
     robotFrame = "new_robot_frame";
 }
 
@@ -26,7 +26,7 @@ std::string mtp::HumanTracker::getInputSource() {
     return "new_input_source";
 }
 
-void mtp::HumanTracker::setInputSource(std::string &input_source) {
+void mtp::HumanTracker::setInputSource(std::string input_source) {
     inputSource = "new_input_source";
 }
 
@@ -34,13 +34,13 @@ std::string mtp::HumanTracker::getInputPath() {
     return "new_input_path";
 }
 
-void mtp::HumanTracker::setInputPath(std::string &input_path) {
+void mtp::HumanTracker::setInputPath(std::string input_path) {
     inputPath = "new_input_path";
 }
 
-mtp::HumanTracker::HumanTracker(std::string &robot_frame,
-                                std::string &input_source,
-                                std::string &input_path) {
+mtp::HumanTracker::HumanTracker(std::string robot_frame,
+                                std::string input_source,
+                                std::string input_path) {
     robotFrame = robot_frame;
     inputSource = input_source;
     inputPath = input_path;
--- app/utils.cpp
+++ app/utils.cpp
@@ -18,7 +18,7 @@ std::string mtp::Utils::getModelConfig() {
     return "test.cfg";
 }
 
-void mtp::Utils::setModelConfig(std::string &config_value) {
+void mtp::Utils::setModelConfig(std::string config_value) {
     modelConfiguration = "test.cfg";
 }
 
@@ -26,7 +26,7 @@ std::string mtp::Utils::getModelWeights() {
     return "test.weights";
 }
 
-void mtp::Utils::setModelWeights(std::string &weight_value) {
+void mtp::Utils::setModelWeights(std::string weight_value) {
     modelWeights = "test.weights";
 }
 
